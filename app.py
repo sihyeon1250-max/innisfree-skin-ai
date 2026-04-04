a0cb3261657eb0f022af9a45f9aaf1878a9c218e
sihyeon1250-max <sihyeon1250@gmail.com>
2026-04-04 17:21:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,5 +10,8 @@ def index():
 def survey():
     return render_template('survey.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    # Render는 'PORT' 환경변수를 사용하므로 이를 읽어와야 합니다.
+    port = int(os.environ.get("PORT", 10000)) 
+    app.run(host='0.0.0.0', port=port)
